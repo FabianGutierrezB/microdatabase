f09ddb50e1a61c25acc7a466a178728bb562d090
felipe mozo <andres.031felipe@hotmail.com>
2022-08-25 21:30:18 -0500

    first commit

---
+++ src/test/java/microdb/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package microdb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
